c9700401031e63cf62f246f1df1d1705757abc21
Ahmed Shaban <139975573+ahmedshaban-blip@users.noreply.github.com>
2025-10-02 12:32:15 +0300

    Update verifyToken.js

--- Middleware/verifyToken.js
+++ Middleware/verifyToken.js
@@ -1,24 +1,9 @@
-// //verifyToken.js
+
 import jwt from "jsonwebtoken"
-// function verifyToken(req, res, next) {
-//     const token = req.cookies.token;
-//     if (!token) {
-//         return res.status(401).send("Access Denied. No Token Provided.");
-//     }
-//     try {
-//         const decoded = jwt.verify(token, 'secretKey');
-//         req.user = decoded;
-//         next();
-//     } catch (err) {
-//         res.status(400).send("Invalid Token");
-//     }
-// }
-// export default verifyToken
 
 
 
 
-// Middleware/verifyToken.js
 
 export default function verifyToken(req, res, next) {
   const auth = req.headers.authorization;    
@@ -26,18 +11,17 @@ export default function verifyToken(req, res, next) {
 
   if (auth?.startsWith('Bearer ')) token = auth.split(' ')[1];
 
-  //
-  if (!token && req.cookies?.token) token = req.cookies.token;
+  
 
   if (!token) return res.status(401).json({ message: 'No token provided' });
 
   try {
-
     const decoded = jwt.verify(token, "secretKey"); 
-    // 
+    
     req.user = { uid: decoded.uid, email: decoded.email };
     return next();
   } catch (e) {
     return res.status(401).json({ message: 'Invalid token' });
   }
 }
+
